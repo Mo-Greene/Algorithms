63c1887225c29ed6a720d0c7e1e3000483a30fc9
Mo-Greene <13blueboy13@daum.net>
2023-01-20 15:39:09 +0900

    1day 1algorithm

--- src/main/java/part1_dataStructure/Queue.java
+++ src/main/java/part1_dataStructure/Queue.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 /**
  * - JAVA ArrayList 클래스를 활용해서 큐를 다루는 enqueue, dequeue 기능 구현해보기
  * - dequeue 기능 호출 시, 큐에 데이터가 없을 경우, null 을 리턴하도록 함
- * - 다양한 데이터 타입을 다룰 수 있도록, Java Genric 타입 문법을 활용해보기
  */
 class Queue<T> {
     private ArrayList<T> queue = new ArrayList<T>();
