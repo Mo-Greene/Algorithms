f5627ade21ad5a26b2bbe2b26b27fd27e2679cb5
Mo-Greene <13blueboy13@daum.net>
2023-01-20 15:37:23 +0900

    1day 1algorithm

---
+++ src/main/java/part1_dataStructure/List.java
@@ -0,0 +1,50 @@
+package part1_dataStructure;
+
+/**
+ * 위 1차원 배열에서, 문자 M 을 가지고 있는 아이템의 수를 출력해보기
+ */
+public class List {
+    public static void main(String[] args) {
+        String dataset[] = {
+                "Braund, Mr. Owen Harris",
+                "Cumings, Mrs. John Bradley (Florence Briggs Thayer)",
+                "Heikkinen, Miss. Laina",
+                "Futrelle, Mrs. Jacques Heath (Lily May Peel)",
+                "Allen, Mr. William Henry",
+                "Moran, Mr. James",
+                "McCarthy, Mr. Timothy J",
+                "Palsson, Master. Gosta Leonard",
+                "Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)",
+                "Nasser, Mrs. Nicholas (Adele Achem)",
+                "Sandstrom, Miss. Marguerite Rut",
+                "Bonnell, Miss. Elizabeth",
+                "Saundercock, Mr. William Henry",
+                "Andersson, Mr. Anders Johan",
+                "Vestrom, Miss. Hulda Amanda Adolfina",
+                "Hewlett, Mrs. (Mary D Kingcome) ",
+                "Rice, Master. Eugene",
+                "Williams, Mr. Charles Eugene",
+                "Vander Planke, Mrs. Julius (Emelia Maria Vandemoortele)",
+                "Masselmani, Mrs. Fatima",
+                "Fynney, Mr. Joseph J",
+                "Beesley, Mr. Lawrence",
+                "McGowan, Miss. Anna",
+                "Sloper, Mr. William Thompson",
+                "Palsson, Miss. Torborg Danira",
+                "Asplund, Mrs. Carl Oscar (Selma Augusta Emilia Johansson)",
+                "Emir, Mr. Farred Chehab",
+                "Fortune, Mr. Charles Alexander",
+                "Dwyer, Miss. Ellen",
+                "Todoroff, Mr. Lalio"
+        };
+
+        int count = 0;
+        for (int i = 0; i < dataset.length; i++) {
+            if (dataset[i].contains("M")) {
+                count += 1;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
---
+++ src/main/java/part1_dataStructure/Queue.java
@@ -0,0 +1,36 @@
+package part1_dataStructure;
+
+import java.util.ArrayList;
+
+/**
+ * - JAVA ArrayList 클래스를 활용해서 큐를 다루는 enqueue, dequeue 기능 구현해보기
+ * - dequeue 기능 호출 시, 큐에 데이터가 없을 경우, null 을 리턴하도록 함
+ * - 다양한 데이터 타입을 다룰 수 있도록, Java Genric 타입 문법을 활용해보기
+ */
+class Queue<T> {
+    private ArrayList<T> queue = new ArrayList<T>();
+
+    public void enqueue(T text) {
+        queue.add(text);
+    }
+
+    public T dequeue() {
+        if (queue.isEmpty()) {
+            return null;
+        } else {
+            return queue.remove(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue1 = new Queue<Integer>();
+        queue1.enqueue(1);
+        queue1.enqueue(2);
+        queue1.enqueue(3);
+        System.out.println(queue1.dequeue());
+        System.out.println(queue1.dequeue());
+        System.out.println(queue1.dequeue());
+        System.out.println(queue1.dequeue());
+    }
+
+}
