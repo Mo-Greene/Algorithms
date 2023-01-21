5e67864d3fe03a89e2f030ed47183f75264e5d75
Mo-Greene <13blueboy13@daum.net>
2023-01-21 15:14:58 +0900

    1day 1algorithm

---
+++ src/main/java/baekjoon/problem/c/c10952.java
@@ -0,0 +1,22 @@
+package baekjoon.problem.c;
+
+import java.util.Scanner;
+
+public class c10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean stop = false;
+        int sum = 0;
+        while (!stop) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum = a + b;
+            if (sum != 0) {
+                System.out.println(sum);
+            } else if (a == 0 && b == 0){
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/baekjoon/problem/c/c8393.java
@@ -0,0 +1,16 @@
+package baekjoon.problem.c;
+
+import java.util.Scanner;
+
+public class c8393 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        System.out.println(result);
+    }
+}
