13e663127c9d32393bdb74f51a84fd66b6b89198
Mo-Greene <13blueboy13@daum.net>
2023-01-22 16:44:58 +0900

    1day 1algo

---
+++ src/main/java/baekjoon/problem/d/d10871.java
@@ -0,0 +1,22 @@
+package baekjoon.problem.d;
+
+import java.util.Scanner;
+
+public class d10871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] < x) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
