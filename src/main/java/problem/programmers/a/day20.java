715e25aafb6b2d96b68cf0c098212834e413e63d
Mo-Greene <13blueboy13@daum.net>
2023-02-19 13:54:45 +0900

    1day 1algo

--- src/main/java/problem/programmers/a/day20.java
+++ src/main/java/problem/programmers/a/day20.java
@@ -2,6 +2,25 @@ package problem.programmers.a;
 
 import java.util.Arrays;
 
+class Solution20_1 {
+    public static void main(String[] args) {
+        Solution20_1 solution20_1 = new Solution20_1();
+        int[][] dots = {{1,1},{2,1},{2,2},{1,2}};
+        System.out.println(solution20_1.solution(dots));
+    }
+    public int solution(int[][] dots) {
+        int w = 0;
+        int h = 0;
+        int x = dots[0][0];
+        int y = dots[0][1];
+        for (int i = 1; i < dots.length; i++) {
+            if (x != dots[i][0]) w = Math.abs(x - dots[i][0]);
+            if (y != dots[i][1]) h = Math.abs(y - dots[i][1]);
+        }
+        return w * h;
+    }
+}
+
 class Solution20_3 {
     public static void main(String[] args) {
         Solution20_3 solution20_3 = new Solution20_3();
