d30902bfdabc4fc3c0d16ef3784cf062d0b472e8
Mo-Greene <13blueboy13@daum.net>
2023-04-01 12:08:00 +0900

    1day 1algo

--- src/main/java/problem/programmers/a/day22.java
+++ src/main/java/problem/programmers/a/day22.java
@@ -1,5 +1,7 @@
 package problem.programmers.a;
 
+import java.util.Arrays;
+
 class Solution22_1 {
     public static void main(String[] args) {
         Solution22_1 solution22_1 = new Solution22_1();
@@ -18,6 +20,44 @@ class Solution22_1 {
 
         return answer;
     }
+}
+
+class Solution22_2 {
+    public static void main(String[] args) {
+//        {{1, 4},
+//            {3, 8},
+//            {9, 2},
+//            {11, 6}}
+        Solution22_2 solution22_2 =new Solution22_2();
+        int[][] dots = {{1, 4},
+                        {3, 8},
+                        {9, 2},
+                        {11, 6}};
+        System.out.println(solution22_2.solution(dots));
+    }
+    public int solution(int[][] dots) {
+        int answer = 0;
+
+        if( (dots[2][1]-dots[0][1])/(dots[2][0]-dots[0][0])+(dots[2][1]-dots[0][1])%(dots[2][0]-dots[0][0]) == (dots[3][1]-dots[1][1])/(dots[3][0]-dots[1][0]) + (dots[3][1]-dots[1][1])%(dots[3][0]-dots[1][0]) ){
+            answer = 1;
+        }
+
+        return answer;
+    }
+
+//        틀린 답
+//        if (dots[0][0] - dots[1][0] == dots[2][0] - dots[3][0] &&
+//                dots[0][1] - dots[1][1] == dots[2][1] - dots[3][1]) {
+//            answer = 1;
+//        }
+//        if (dots[0][0] - dots[2][0] == dots[1][0] - dots[3][0] &&
+//                dots[0][1] - dots[2][1] == dots[1][1] - dots[3][1]) {
+//            answer = 1;
+//        }
+//        if (dots[0][0] - dots[3][0] == dots[1][0] - dots[2][0] &&
+//                dots[0][1] - dots[3][1] == dots[1][1] - dots[2][1]) {
+//            answer = 1;
+//        }
 }
 
 public class day22 {
