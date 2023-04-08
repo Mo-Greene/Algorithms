69b06be014fcce1993de2d311de1be9f3fc32871
Mo-Greene <13blueboy13@daum.net>
2023-04-08 13:14:27 +0900

    1day 1algo

--- src/main/java/problem/programmers/a/day22.java
+++ src/main/java/problem/programmers/a/day22.java
@@ -1,7 +1,5 @@
 package problem.programmers.a;
 
-import java.util.Arrays;
-
 class Solution22_1 {
     public static void main(String[] args) {
         Solution22_1 solution22_1 = new Solution22_1();
@@ -38,7 +36,8 @@ class Solution22_2 {
     public int solution(int[][] dots) {
         int answer = 0;
 
-        if( (dots[2][1]-dots[0][1])/(dots[2][0]-dots[0][0])+(dots[2][1]-dots[0][1])%(dots[2][0]-dots[0][0]) == (dots[3][1]-dots[1][1])/(dots[3][0]-dots[1][0]) + (dots[3][1]-dots[1][1])%(dots[3][0]-dots[1][0]) ){
+        if( (dots[2][1]-dots[0][1])/(dots[2][0]-dots[0][0])+(dots[2][1]-dots[0][1])%(dots[2][0]-dots[0][0]) ==
+                (dots[3][1]-dots[1][1])/(dots[3][0]-dots[1][0]) + (dots[3][1]-dots[1][1])%(dots[3][0]-dots[1][0]) ){
             answer = 1;
         }
 
