b5a81c7df11175e4a2e818a1a64c5721922e5b6d
Mo-Greene <13blueboy13@daum.net>
2023-05-22 11:59:44 +0900

    프로그래머스 algorithm

--- src/main/java/problem/programmers/level1/page1.java
+++ src/main/java/problem/programmers/level1/page1.java
@@ -1,5 +1,7 @@
 package problem.programmers.level1;
 
+import java.util.Arrays;
+
 class Solution1 {
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
@@ -54,6 +56,131 @@ class Solution3 {
             }
         }
         return n != 0 ? answer : 0;
+    }
+}
+
+class Solution4 {
+    public static void main(String[] args) {
+        Solution4 solution4 = new Solution4();
+        int n = 12;
+        System.out.println(solution4.solution(n));
+    }
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+
+class Solution5 {
+    public static void main(String[] args) {
+        Solution5 solution5 = new Solution5();
+        String s = "hello";
+        System.out.println(solution5.solution(s));
+    }
+    boolean solution(String s) {
+        int pCnt = 0;
+        int yCnt = 0;
+        String str = s.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'p') {
+                pCnt++;
+            } else if (str.charAt(i) == 'y') {
+                yCnt++;
+            } else if (!str.contains("p") && !str.contains("y")){
+                return true;
+            }
+        }
+
+        return pCnt == yCnt;
+    }
+}
+
+class Solution6 {
+    public static void main(String[] args) {
+        Solution6 solution6 = new Solution6();
+        long n = 12345;
+        System.out.println(Arrays.toString(solution6.solution(n)));
+    }
+    public int[] solution(long n) {
+        String str = String.valueOf(n);
+
+        int[] answer = new int[str.length()];
+        int count = 0;
+
+        while (n != 0) {
+            long sum = n % 10;
+            answer[count] = (int) sum;
+            count++;
+            n = n / 10;
+        }
+
+        return answer;
+    }
+}
+
+class Solution7 {
+    public static void main(String[] args) {
+        Solution7 solution7 = new Solution7();
+        long n = 121;
+        System.out.println(solution7.solution(n));
+    }
+    public long solution(long n) {
+        long answer = 0;
+        float half = (float) Math.sqrt(n);
+
+        for (int i = 0; i <= half; i++) {
+            if (Math.pow(i, 2) == n) {
+                answer = (long) Math.pow(i + 1, 2);
+                break;
+            } else {
+                answer = -1;
+            }
+        }
+
+        return answer;
+    }
+}
+
+class Solution8 {
+    public static void main(String[] args) {
+        Solution8 solution8 = new Solution8();
+        String s = "1234";
+        System.out.println(solution8.solution(s));
+    }
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
+
+class Solution9 {
+    public static void main(String[] args) {
+        Solution9 solution9 = new Solution9();
+        long n = 118372;
+        System.out.println(solution9.solution(n));
+    }
+    public long solution(long n) {
+        long answer = 0;
+
+        String[] digits = String.valueOf(n).split("");
+        Arrays.sort(digits, (a, b) -> b.compareTo(a));
+
+        StringBuilder sb = new StringBuilder();
+        for (String digit : digits) {
+            sb.append(digit);
+        }
+
+        answer = Long.parseLong(String.valueOf(sb));
+
+        return answer;
     }
 }
 
