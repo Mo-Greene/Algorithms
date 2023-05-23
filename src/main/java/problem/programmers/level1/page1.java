d036aad2d1734dee23c59610530ad5d7d58c416b
Mo-Greene <13blueboy13@daum.net>
2023-05-23 13:15:54 +0900

    프로그래머스 algorithm

--- src/main/java/problem/programmers/level1/page1.java
+++ src/main/java/problem/programmers/level1/page1.java
@@ -1,6 +1,9 @@
 package problem.programmers.level1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class Solution1 {
     public static void main(String[] args) {
@@ -180,6 +183,196 @@ class Solution9 {
 
         answer = Long.parseLong(String.valueOf(sb));
 
+        return answer;
+    }
+}
+
+class Solution10 {
+    public static void main(String[] args) {
+        Solution10 solution10 = new Solution10();
+        int x = 12;
+        System.out.println(solution10.solution(x));
+    }
+    public boolean solution(int x) {
+        boolean answer;
+        int sum = 0;
+        String str = String.valueOf(x);
+        for (int i = 0; i < str.length(); i++) {
+            sum += str.charAt(i) - 48;
+        }
+
+        if (x % sum == 0) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
+    }
+}
+
+class Solution11 {
+    public static void main(String[] args) {
+        Solution11 solution11 = new Solution11();
+        int a = 5;
+        int b = 3;
+        System.out.println(solution11.solution(a, b));
+    }
+    public long solution(int a, int b) {
+        long answer = 0;
+        int[] arr = {a, b};
+        Arrays.sort(arr);
+        int min = arr[0];
+        int max = arr[1];
+
+        for (int i = min; i <= max; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
+
+class Solution12 {
+    public static void main(String[] args) {
+        Solution12 solution12 = new Solution12();
+        int num = 6;
+        System.out.println(solution12.solution(num));
+    }
+    public int solution(int num) {
+        int cnt = 0;
+        if (num == 1) {
+            return 0;
+        }
+        long parsing = num;
+
+        for (int i = 1; i <= 500; i++) {
+            if (parsing == 1) {
+                return cnt;
+            }
+            if (parsing % 2 == 0) {
+                parsing = even(parsing);
+            } else {
+                parsing = odd(parsing);
+            }
+            cnt++;
+        }
+
+        if (cnt == 500) {
+            return -1;
+        }
+
+        return cnt;
+    }
+
+    private long even(long num) {
+        return num / 2;
+    }
+
+    private long odd(long num) {
+        return (num * 3) + 1;
+    }
+}
+
+class Solution13 {
+    public static void main(String[] args) {
+        Solution13 solution13 = new Solution13();
+        String[] seoul = {"Jane", "Kim"};
+        System.out.println(solution13.solution(seoul));
+    }
+    public String solution(String[] seoul) {
+        int kimLocation = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                kimLocation = i;
+            }
+        }
+        return "김서방은 " + kimLocation + "에 있다";
+    }
+}
+
+class Solution14 {
+    public static void main(String[] args) {
+        Solution14 solution14 = new Solution14();
+        int[] arr = {10, 9, 7, 5};
+        int divisor = 5;
+        System.out.println(Arrays.toString(solution14.solution(arr, divisor)));
+    }
+    public int[] solution(int[] arr, int divisor) {
+        List<Integer> arrList = new ArrayList<>();
+        int[] noSize = {-1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                arrList.add(arr[i]);
+            }
+        }
+        if (arrList.size() == 0) {
+            return noSize;
+        }
+
+        int[] answer = new int[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
+            answer[i] = arrList.get(i);
+        }
+        Arrays.sort(answer);
+
+        return answer;
+    }
+}
+
+class Solution15 {
+    public static void main(String[] args) {
+        Solution15 solution15 = new Solution15();
+        String phoneNumber = "01033334444";
+        System.out.println(solution15.solution(phoneNumber));
+    }
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder(phone_number);
+        String reverse = sb.reverse().toString();
+        String[] arr = reverse.split("");
+
+        for (int i = 4; i < arr.length; i++) {
+            arr[i] = arr[i].replace(arr[i], "*");
+        }
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb2.append(arr[i]);
+        }
+
+        return sb2.reverse().toString();
+    }
+}
+
+class Solution16 {
+    public static void main(String[] args) {
+        Solution16 solution16 = new Solution16();
+        int[] absolutes = {4, 7, 12};
+        boolean[] signs = {true, false, true};
+        System.out.println(solution16.solution(absolutes, signs));
+    }
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
+
+class Solution17 {
+    public static void main(String[] args) {
+        Solution17 solution17 = new Solution17();
+        int[] numbers = {5,8,4,0,6,7,9};
+        System.out.println(solution17.solution(numbers));
+    }
+    public int solution(int[] numbers) {
+        int answer = 45;
+
+        for (int i = 0; i < numbers.length; i++) {
+            answer -= numbers[i];
+        }
         return answer;
     }
 }
