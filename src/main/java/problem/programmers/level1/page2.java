adffec0f1bc52bd7154c4142e5ee8c7c44b2af20
Mo-Greene <13blueboy13@daum.net>
2023-05-25 14:34:14 +0900

    프로그래머스 algorithm

--- src/main/java/problem/programmers/level1/page2.java
+++ src/main/java/problem/programmers/level1/page2.java
@@ -94,5 +94,36 @@ class Solution22 {
         return answer;
     }
 }
+
+class Solution23 {
+    public static void main(String[] args) {
+        Solution23 solution23 = new Solution23();
+        int left = 24;
+        int right = 27;
+        System.out.println(solution23.solution(left, right));
+    }
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        while (left <= right) {
+            int count = 0;
+            for (int j = 1; j <= left; j++) {
+                if (left % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count % 2 == 0) {
+                answer += left;
+            } else {
+                answer -= left;
+            }
+            left++;
+        }
+
+        return answer;
+    }
+}
+
 public class page2 {
 }
